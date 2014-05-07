fe49944b48a1a4ee0bfc69167aa8be1351ce3661
xwj徐文杰 <wenjiexu@ctrip.com>
2014-05-07 13:12:02 +0800

    add another function, computer sum of num

--- sayHello.py
+++ sayHello.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 def sayHello(name):
     print 'Welcome '+name+' come to python world!'
-    print ' finish'
+
+def Sum(num):
+    i=0
+    sum=0
+    for i in range(num):
+        sum +=i
+    return sum
 if __name__ == "__main__":
     sayHello('wenjie')
 
