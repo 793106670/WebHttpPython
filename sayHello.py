e74054502690a5999420db40283307f4fc021db2
xwj徐文杰 <wenjiexu@ctrip.com>
2014-05-07 11:13:25 +0800

    use another user to commit

--- sayHello.py
+++ sayHello.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-print 'hello git world!'
+def sayHello(name):
+    print 'Welcome '+name+' come to python world!'
+    print ' finish'
+if __name__ == "__main__":
+    sayHello('wenjie')
 
